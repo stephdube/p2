3af53ad455586c5eea35cc65faabfa18881eb231
Steph Dube <scdube7@gmail.com>
2014-07-06 18:25:15 -0400

    made further styling tweaks

--- index.php
+++ index.php
@@ -9,20 +9,23 @@
 		<h1>Password Generator</h1>
 		<div class="container">
 			<form method="POST" action="index.php">
-				How many words: <select name="word_count">
+				How many words? <select name="word_count" class="select">
 					<?php for ($i = 1; $i < 10; $i++):?>
 						<option value="<?php echo $i;?>"><?php echo $i;?></option>
 					<?php endfor;?>
 				</select></br>
-				Include numerical: <input type="checkbox" name="number"><br>
-				Inlude special symbol: <input type="checkbox" name="special"><br>
-				First letters capitalized:<input type="checkbox" name="caps"> <br>
-				<input type="submit" value="Submit">
+				Include a number? <input type="checkbox" name="number"><br>
+				Inlude a special symbol? <input type="checkbox" name="special"><br>
+				Capitalize first letter in words?<input type="checkbox" name="caps"> <br><br>
+				<input type="submit" value="Submit" class="input"/>
 			</form>
 		</div>
 		<h2>Your password:</h3>
 		<div class="container">
-			<b><?php echo $password?></b>
+			<p id="pass"><b><?php echo $password?></b></p>
 		</div>
 	</body>
+	<footer>
+		By Stephanie Dube (7/2014) for <a href="http://dwa15.com/">DWA-15</a>
+	</footer>
 </html>
