f80e8e2b68b2845b9bae59b501ef034cf93f21ae
Steph Dube <scdube7@gmail.com>
2014-07-06 18:34:56 -0400

    fixed error in logic (shouldn't try to execute if POST is empty)

--- password_logic.php
+++ password_logic.php
@@ -3,27 +3,29 @@
 	
 	$password = '';
 
-	// grab as many words as requested from the word list at random indexes
-	for ($i=0; $i<$_POST["word_count"]; $i++){
-		$password .= ' '.$words[rand(0, count($words) - 1)];
-	}
+	if (array_key_exists("word_count", $_POST)){
 
-	// capitalize the first letter of each word
-	if (array_key_exists("caps", $_POST)){
-		$password = ucwords($password);
-	}
+		// grab as many words as requested from the word list at random indexes
+		for ($i=0; $i<$_POST["word_count"]; $i++){
+			$password .= ' '.$words[rand(0, count($words) - 1)];
+		}
 
-	// add a random number between 0 and 9 somewhere in the password
-	if (array_key_exists("number", $_POST)){
-		$password = substr_replace($password, rand(0, 9), 
-			rand(0, strlen($password) - 1), 0);
-	}
+		// capitalize the first letter of each word
+		if (array_key_exists("caps", $_POST)){
+			$password = ucwords($password);
+		}
 
-	// add a random special character  somewhere in the password
-	if (array_key_exists("special", $_POST)){
-		$specials = array('!', '@', '#', '$', '%', '^', '&', '*');
-		$password = substr_replace($password, 
-			$specials[rand(0, count($specials) - 1)], 
-			rand(0, strlen($password) - 1), 0);
-	}
+		// add a random number between 0 and 9 somewhere in the password
+		if (array_key_exists("number", $_POST)){
+			$password = substr_replace($password, rand(0, 9), 
+				rand(0, strlen($password) - 1), 0);
+		}
 
+		// add a random special character  somewhere in the password
+		if (array_key_exists("special", $_POST)){
+			$specials = array('!', '@', '#', '$', '%', '^', '&', '*');
+			$password = substr_replace($password, 
+				$specials[rand(0, count($specials) - 1)], 
+				rand(0, strlen($password) - 1), 0);
+		}
+	}
